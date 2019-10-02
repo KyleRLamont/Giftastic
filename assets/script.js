4c97bdc805bdee3add29e268258360c6e1f7effa
Kyle LaMont <kylerlamont@gmail.com>
2019-10-02 09:43:26 -0700

    added animalbutton functionality to pull gifs from API and add them to dom

--- assets/script.js
+++ assets/script.js
@@ -17,11 +17,8 @@ renderButtons();
 
 $("#animalbtn").on("click", function(event) {
     event.preventDefault();
-
     var newanimal = $("#animaladd").val().trim();
-
     animals.push(newanimal);
-
     renderButtons();
     $("form").trigger("reset");
   });
@@ -29,13 +26,28 @@ $("#animalbtn").on("click", function(event) {
 $(document).on("click", ".animal", displaygifs);
     function displaygifs(){
         var searchterm = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Bk5QuTfcpPza5xYFYcMKbn1rl0kIDzw6&q=" + searchterm + "&limit=10&offset=0&rating=G&lang=en"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Bk5QuTfcpPza5xYFYcMKbn1rl0kIDzw6&q=" + searchterm + "&limit=10&offset=0&lang=en"
         
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response)
+            console.log(response);
+            var responseedit = response.data;
+            console.log(responseedit);
+            for (var i = 0; i < responseedit.length; i++){
+                var animalDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + responseedit[i].rating);
+                var animgif = $("<img>");
+                    animgif.attr("src", responseedit[i].images.fixed_height_small_still.url);
+                    animgif.attr("data-still", responseedit[i].images.fixed_height_small_still.url);
+                    animgif.attr("data-animate", responseedit[i].images.fixed_height_small.url);
+                    animgif.attr("data-state", "still");
+                    animgif.addClass("gif");
+                animalDiv.append(p);
+                animalDiv.append(animgif);
+                $("#resultgif").prepend(animalDiv);
+            }
         });
     };
 });
